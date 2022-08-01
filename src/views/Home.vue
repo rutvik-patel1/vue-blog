9db18e2e3d0554b35aaaa51ea46981e473bbcea4
Rutvik Patel <rutvik.patel@bacancy.com>
2022-08-01 15:25:25 +0530

    refactor: search functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,92 +3,88 @@
     <nav-bar></nav-bar>
     <blog-container>
       <template v-slot:main>
-          <base-card v-for="data in blogs" :key="data.id" :blog="data"></base-card>
-          <div v-if="blogs.length === 0">No data found..! Try Something else....</div>
+        <base-card
+          v-for="data in blogs"
+          :key="data.id"
+          :blog="data"
+        ></base-card>
+        <div v-if="blogs.length === 0">
+          No data found..! Try Something else....
+        </div>
       </template>
       <template v-slot:sidebar>
-          <base-search></base-search>
-          <base-category></base-category>
+        <base-search></base-search>
+        <base-category></base-category>
       </template>
     </blog-container>
   </div>
 </template>
 
 <script>
-import NavBar from '../components/NavBar.vue'
-import BlogContainer from '../components/BlogContainer.vue'
-import BaseCard from '../components/BaseCard.vue'
-import BaseSearch from '../components/BaseSearch.vue'
-import BaseCategory from '../components/BaseCategory.vue'
-import {getAllBlog} from '../api/blog'
+import NavBar from "../components/NavBar.vue";
+import BlogContainer from "../components/BlogContainer.vue";
+import BaseCard from "../components/BaseCard.vue";
+import BaseSearch from "../components/BaseSearch.vue";
+import BaseCategory from "../components/BaseCategory.vue";
+import { getAllBlog } from "../api/blog";
 
 export default {
-  name: 'Home',
-  components:{
+  name: "Home",
+  components: {
     NavBar,
     BlogContainer,
     BaseCard,
     BaseSearch,
-    BaseCategory
+    BaseCategory,
   },
-  created(){
-    this.fetchBlogs()
-   },
+  created() {
+    this.fetchBlogs();
+  },
   data() {
     return {
-      allBlogs:[],
-      categoryBlogs:[],
-      searchBlogs:[],
-      category:'',
-      search:''
-    }
+      allBlogs: [],
+      category: "",
+      search: "",
+    };
   },
-  methods:{
-    fetchBlogs(){
+  methods: {
+    fetchBlogs() {
       getAllBlog()
-    .then((res)=>{
-      this.allBlogs = res.data
-      this.category = this.$route.query.category
-      this.search = this.$route.query.search
-    })
-    .catch((err)=>{
-      console.log("err",err)
-    })
-    }
+        .then((res) => {
+          this.allBlogs = res.data;
+          this.category = this.$route.query.category;
+          this.search = this.$route.query.search;
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+    },
   },
-  computed:{
-     blogs(){
-       
-      if(this.category){
-        return this.allBlogs.filter(each=>each.category === this.category)
+  computed: {
+    blogs() {
+      if (this.category) {
+        return this.allBlogs.filter((each) => each.category === this.category);
       }
-      if(this.search){
-        return this.allBlogs.filter(each=> each.author.toLowerCase().includes(this.search.toLowerCase()) || each.category.toLowerCase().includes(this.search.toLowerCase()) || each.content.toLowerCase().includes(this.search.toLowerCase()) || each.title.toLowerCase().includes(this.search.toLowerCase()))
+      if (this.search) {
+        return this.allBlogs.filter(
+          (each) =>
+            each.title.toLowerCase().includes(this.search.toLowerCase())||
+            each.author.toLowerCase().includes(this.search.toLowerCase()) ||
+            each.category.toLowerCase().includes(this.search.toLowerCase()) ||
+            each.content.toLowerCase().includes(this.search.toLowerCase())
+        );
       }
-      return this.allBlogs 
-      }
+      return this.allBlogs;
+    },
   },
-  watch:{
-    $route:{
-        handler:function(to){
-        this.category = to.query.category
-        this.search = to.query.search
-        // console.log("chnged")
-        // if(qCategory){
-        //   console.log("category changed")
-        //   this.categoryBlogs = this.allBlogs.filter(each=>each.category === qCategory)
-        //   console.log(this.categoryBlogs)
-        // }
-        // if(qSearch){
-        //   console.log("search changed")
-        //   this.searchBlogs = this.allBlogs.filter(each=> each.author.includes(qSearch) || each.title.includes(qSearch))
-        //   console.log(this.categoryBlogs)
-        // }
-        },
-        deep:true
-
-    } 
-
-  }
-}
+  watch: {
+    $route: {
+      handler: function (to) {
+        this.category = to.query.category;
+        this.search = to.query.search;
+      },
+      deep: true,
+    },
+  },
+};
 </script>
